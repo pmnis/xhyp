ddbb926174ab4040770907de5d8d7c5626ebece0
Pierre Morel <pmorel@mnis.fr>
2020-05-21 19:26:39 +0200

    virtio: access virtio device
    Signed-off-by: Pierre Morel <pmorel@mnis.fr>

--- drivers/virtio-serial/virtio_serial.c
+++ drivers/virtio-serial/virtio_serial.c
@@ -1,20 +1,32 @@
+#include <xhyp/config.h>
 #include <xhyp/shared_page.h>
 #include <sys/xhyp.h>
-//#include <xhyp/irq.h>
 #include <xhyp/stdlib.h>
-//#include <xhyp/domains.h>
 #include <xhyp/hyp.h>
-//#include <xhyp/event.h>
-
-#include <xhyp/arinc.h>
 
 #include <autoconf.h>
 
-#include "hyp.h"
+unsigned long periph_base = PERIPH_BASE;
+
+#define VIRTIO_00_OFFSET	0x00208000
+struct virtio_registers {
+	unsigned int reg0;
+	unsigned int reg1;
+	unsigned int reg2;
+	unsigned int reg3;
+	unsigned int reg4;
+};
+
+struct virtio_registers	*virtio_p = (struct virtio_registers *) (PERIPH_BASE + VIRTIO_00_OFFSET);
 
 struct shared_page *xhyp_sp = (struct shared_page *) (0x02000000);
 
 #define wait_next_period()		_hyp_idle()
+
+char buffer[80];
+#define write(fmt, ...) \
+	sprintf(buffer, fmt, ## __VA_ARGS__); \
+	_hyp_console(buffer, strlen(buffer));
 
 #define MAX     10000000
 static void delay(unsigned long max)
@@ -28,12 +40,17 @@ static void delay(unsigned long max)
                         while (k++ < MAX);
 }
 
+int serial_write(const char *s, int cnt)
+{
+	return 0;
+}
+
 void start_kernel(void)
 {
-	_hyp_console("virtio user\n", 13);
+	write("virtio_serial %08x\n", &virtio_p->reg0);
 
 	while(1) {
-		_hyp_console("virtio_user\n", 13);
+		write("virtio_serial %08x\n", virtio_p->reg0);
 		delay(1000);
 	}
 }
