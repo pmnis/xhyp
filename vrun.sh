34e34ccbb6b5f57ef85609012b7152bdb497bd2d
Pierre Morel <pmorel@mnis.fr>
2020-05-24 15:00:50 +0200

    start script: add virtoi backend
    Signed-off-by: Pierre Morel <pmorel@mnis.fr>

--- vrun.sh
+++ vrun.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
 
-qemu-system-arm -M versatilepb -kernel xhyp -nographic  -device virtio-serial-device
+qemu-system-arm \
+	-M versatilepb \
+	-kernel xhyp \
+	-nographic \
+	-chardev file,id=s11,server,nowait,path=/tmp/xhyp_log \
+	-device virtio-serial-device,id=virtio-serial0,max_ports=16 \
+	-device virtserialport,chardev=s11,name=org.linux-kvm.port.11,bus=virtio-serial0.0,id=port11
+
+exit 0
+
+	-device_add virtio-serial-pci,id=virtio-serial0,max_ports=16,bus=pci.0,addr=0x8
+	-chardev socket,id=s11,path=/tmp/tt11,server,nowait \
+	-chardev stdio,id=s11,server,nowait \
+	-nodefaults \
